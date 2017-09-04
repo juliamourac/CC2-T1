aec5e479f7005c82655c9c852a09fe0ecc7193e0
WellitonAlt <etoalt@hotmail.com>
2017-09-03 21:57:03 -0300

    Adicionado os aquivos de Tabela de Simbolos

---
+++ src/ErrorListener.java
@@ -0,0 +1,38 @@
+import java.util.BitSet;
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+public class ErrorListener implements ANTLRErrorListener {
+
+    SaidaParser sp;
+
+    public ErrorListener(SaidaParser sp) {
+        this.sp = sp;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> rcgnzr, Object o, int i, int i1, String string, RecognitionException re) {
+        if (!sp.isModificado()) {
+            sp.println("Linha " + i + ": errro sintatico proximo a " +string);
+        }
+    }
+
+    @Override
+    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean bln, BitSet bitset, ATNConfigSet atncs) {
+        if (!sp.isModificado()) {
+            sp.println("Ambiguidade: linha " + i + ":" + i1);
+        }
+    }
+
+    @Override
+    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitset, ATNConfigSet atncs) {
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atncs) {
+    }
+}
---
+++ src/Tipo.java
@@ -0,0 +1,3 @@
+public enum Tipo {
+    VARIAVEL, FUNCAO
+}
